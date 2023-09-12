6420c25ee05835a637257eb40862f4008f41bf03
yuulye <yuulye@gmail.com>
2023-09-12 12:53:09 +0700

    update scrape

--- cli/scrape.mjs
+++ cli/scrape.mjs
@@ -1,10 +1,36 @@
 import { PlaywrightCrawler } from 'crawlee';
 
+function trim(items) {
+  for (let i = 0; i < items.length; i++) {
+    items[i] = new Item(items[i].trim());
+  }
+  return items;
+}
+
+class Item {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Type {
+  constructor(name, items) {
+    this.name = name;
+    this.items = trim(items);
+  }
+}
+
 const crawler = new PlaywrightCrawler({
   async requestHandler({ page }) {
     const locator = page.locator('.tabs6 > li > a');
-    const items = await locator.allTextContents();
-    console.log(items);
+    const types = await locator.allTextContents();
+    for (let i = 0; i < types.length; i++) {
+      const items = await page.locator(
+        `.content${i+1} .gallerytext`
+      ).allInnerTexts();
+      types[i] = new Type(types[i], items);
+      console.log(types[i]);
+    }
   },
 });
 
