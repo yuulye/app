70cfe619815b7edfbe0f28dbcc2df71e24892c6f
yuulye <yuulye@gmail.com>
2023-09-13 17:27:58 +0700

    update fetched data

--- cli/scrape.mjs
+++ cli/scrape.mjs
@@ -1,22 +1,27 @@
 import { PlaywrightCrawler } from 'crawlee';
 
-function trim(items) {
+async function process(items) {
   for (let i = 0; i < items.length; i++) {
-    items[i] = new Item(items[i].trim());
+    const name = await items[i].getAttribute(`title`);
+    const link = await items[i].getAttribute(`href`);
+    const img = await items[i].locator(`img`).getAttribute(`src`);
+    items[i] = new Item(name, link, img);
   }
   return items;
 }
 
 class Item {
-  constructor(name) {
+  constructor(name, link, img) {
     this.name = name;
+    this.link = link;
+    this.img = img;
   }
 }
 
 class Type {
   constructor(name, items) {
     this.name = name;
-    this.items = trim(items);
+    this.items = items;
   }
 }
 
@@ -25,9 +30,9 @@ const crawler = new PlaywrightCrawler({
     const locator = page.locator('.tabs6 > li > a');
     const types = await locator.allTextContents();
     for (let i = 0; i < types.length; i++) {
-      const items = await page.locator(
-        `.content${i+1} .gallerytext`
-      ).allInnerTexts();
+      const items = await process(await page.locator(
+        `.content${i+1} .gallery .gallerybox .thumb a`
+      ).all());
       types[i] = new Type(types[i], items);
       console.log(types[i]);
     }
