94ede259a0d4e2a8b96cc551363c6991f6211ec3
yuulye <yuulye@gmail.com>
2023-10-01 12:20:48 +0700

    update

--- views/js/main.js
+++ views/js/main.js
@@ -2,11 +2,9 @@ function item(i) {
   el = document.getElementById('draggable-'+i)
   el.style.display = 'block';
   $(el).mousedown();
-  console.log(items[i], el);
 }
 
 function closePopup(i) {
-  console.log(items[i]);
   el = document.getElementById('draggable-'+i)
   el.style.display = 'none';
 }
