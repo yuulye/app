765a88e3b2564e3e1f949b7b101e6e425009d4e7
yuulye <yuulye@gmail.com>
2023-10-03 01:23:08 +0700

    [implement] item details download image

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testRouter = require('./routes/test');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- views/js/main.js
+++ views/js/main.js
@@ -1,4 +1,7 @@
+let itemID = 0;
+
 function item(i) {
+  itemID = i;
   el = document.getElementById('draggable-'+i)
   el.style.display = 'block';
   $(el).mousedown();
@@ -7,4 +10,17 @@ function item(i) {
 function closePopup(i) {
   el = document.getElementById('draggable-'+i)
   el.style.display = 'none';
+}
+
+function saveImage() {
+  const element = document.getElementById("draggable-"+itemID);
+  const canvas = document.getElementById("my-canvas");
+
+  html2canvas(element).then(function(canvas) {
+    const dataURL = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.download = "item.png";
+    link.href = dataURL;
+    link.click();
+  });
 }
