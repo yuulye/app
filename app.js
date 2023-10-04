946f9eed582a284e5a3d02474a20ed6d480ad0d3
yuulye <yuulye@gmail.com>
2023-10-04 12:18:59 +0700

    [implement] init discord bot, util, config

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('./discord/bot');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
---
+++ util.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function checkFileExistsSync(filepath) {
+  console.log(`checking filepath ${filepath}`);
+  if (fs.existsSync(filepath)) return true;
+  return false;
+}
+
+const localConfigPath = 'config.json';
+
+exports.config = () => {
+  let data = {
+    discord: {
+      bot: {
+        token: "yangmrogramorangnyaganteng"
+      }
+    }
+  };
+  if (!checkFileExistsSync(`./${localConfigPath}`)) {
+    console.log(`File config.json is not exists!`);
+    console.log(`Checking process.env.config...`);
+    if (process.env.config) {
+      console.log(`Using process.env.config...`);
+      data = process.env.config;
+    } else {
+      console.log(`couldn't find any config!`);
+    }
+  } else {
+    console.log(`Using config.json...`);
+    data = require(`./${localConfigPath}`);
+  }
+  return  data;
+};
+
