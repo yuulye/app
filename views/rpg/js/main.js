a780a3254b09460e12d4e888b398392a7dc5be6c
yuulye <yuulye@gmail.com>
2023-12-14 02:35:09 +0700

    update

--- views/rpg/js/main.js
+++ views/rpg/js/main.js
@@ -1,10 +1,13 @@
-const bgPos = {x: -10, y: -10, id: 0};
+const bgEl = document.getElementById('bg');
+const bgPos = {x: -10, y: -60, id: 0};
 const bgWidth = 490;
 const bgHeight = 266;
 const bgCount = 38;
 const bgCol = 5;
+
+bgEl.style.animationPlayState = 'paused';
+
 function changeBG() {
-  const bg = document.getElementById('bg');
   console.log(bg);
   bgPos.x -= bgWidth;
   if (bgPos.id < bgCount - 1) {
@@ -15,7 +18,8 @@ function changeBG() {
     }
   } else {
     bgPos.id = 0;
-    bgPos.x = bgPos.y = -10;
+    bgPos.x = -10;
+    bgPos.y = -60;
   }
   bg.style.backgroundPositionX = `${bgPos.x}px`;
   bg.style.backgroundPositionY = `${bgPos.y}px`;
@@ -26,20 +30,69 @@ let toggle = true;
 const playerEl = document.getElementById('char');
 const player = {x: -4, y: -4, id: 0, pose: 0};
 const playerCount = 6;
+const poseCount = 5;
+const idlePose = {
+  counter: 0, pos: [
+    -4, -324, -284, -324, -4, -244, -244, -244, -244
+  ],
+  delay: 2, delayCounter: 0
+};
 
-const poseCount = 4;
-function changePose() {
-  player.pose++;
-  if (player.pose < poseCount) {
-    if (player.pose == 1) {
-      player.x = -240;
-    } else if (player.pose == 2) {
-      player.x = -360;
-    }
+function walk(self) {
+  console.log(self);
+  const text = self.getElementsByTagName('h3')[0].innerHTML;
+  if (text === 'Walk') {
+    self.getElementsByTagName('h3')[0].innerHTML = 'Stop';
+    changePose(4);
   } else {
-    player.pose = 0;
+    self.getElementsByTagName('h3')[0].innerHTML = 'Walk';
+    changePose(0);
   }
 }
+
+function changePose(pose = true) {
+  bgEl.style.animationPlayState = 'paused';
+  if (pose === true) player.pose++;
+  else player.pose = pose;
+  console.log(pose);
+  if (player.pose < poseCount) {
+    if (player.pose == 0) {
+      player.x = -4;
+      idlePose.counter = 0;
+      idlePose.delayCounter = 0;
+    } else if (player.pose == 1) {
+      player.x = -244;
+    } else if (player.pose == 2) {
+      player.x = -364;
+    } else if (player.pose == 4) {
+      bgEl.style.animationPlayState = 'running';
+    }
+  } else {
+    changePose(0);
+  }
+}
+
+setInterval(function() {
+  if (player.pose == 0) {
+    if (idlePose.delayCounter === idlePose.delay) {
+      if (idlePose.counter === idlePose.pos.length) {
+        idlePose.counter = 0;
+      }
+      playerEl.style.backgroundPositionX
+        = `${idlePose.pos[idlePose.counter]}px`;
+      idlePose.counter++;
+      idlePose.delayCounter = 0;
+    }
+    idlePose.delayCounter++;
+  } else if (player.pose == 1 || player.pose == 2) {
+    playerEl.style.backgroundPositionX = `${player.x}px`;
+  } else if (player.pose == 3) {
+    playerEl.style.backgroundPositionX = `${toggle?-404:-444}px`;
+  } else if (player.pose == 4) {
+    playerEl.style.backgroundPositionX = `${toggle?-4:-44}px`;
+  }
+  toggle = !toggle;
+}, 500);
 
 function changePlayer() {
   player.id++;
@@ -51,14 +104,3 @@ function changePlayer() {
   }
   playerEl.style.backgroundPositionY = `${player.y}px`;
 }
-
-setInterval(function() {
-  if (player.pose == 0) {
-    playerEl.style.backgroundPositionX = `${toggle?-4:-44}px`;
-  } else if (player.pose == 1 || player.pose == 2) {
-    playerEl.style.backgroundPositionX = `${player.x}px`;
-  } else if (player.pose == 3) {
-    playerEl.style.backgroundPositionX = `${toggle?-404:-444}px`;
-  }
-  toggle = !toggle;
-}, 500);
